e04d2434d79b2a8456522ce91b51b9aa23539d4d
miner <minerhossain1033@gmail.com>
2024-10-03 01:59:20 +0600

    second  commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,11 +18,20 @@ class Myapp extends StatelessWidget {//type stl then enter
 }
 class HomeActivity extends StatelessWidget {
   const HomeActivity({super.key});
-
+MySnackBar(message,context)
+{
+  return ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));//return-showsnackbar--Snackbar
+}
   @override
   Widget build(BuildContext context) {//care full using comma
     return Scaffold(
+
       appBar: AppBar(
+        actions: [
+          IconButton(onPressed: (){MySnackBar("i am comment",context);}, icon: Icon(Icons.comment)),//(){er vitore MySnackbar();}icon :Icon
+          IconButton(onPressed: (){MySnackBar("i am mail",context);}, icon: Icon(Icons.mail)),
+
+        ],
         backgroundColor: Colors.blueAccent,
         title: Text("hi from appbar"),),
           body: Text("Assalamulaikum"),
